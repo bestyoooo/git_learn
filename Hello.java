5ec1270d94b3d64967b58881526ad916931418f4
wujianxiang <bestyoooo@163.com>
2023-09-05 11:40:23 +0800

    Test Hello commit

--- Hello.java
+++ Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	System.out.println("Dev Hello");
 	System.out.println("Master new merge");
 	System.out.println("Dev new merge");
+	System.out.println("Test Hello");
     }
 }
 
