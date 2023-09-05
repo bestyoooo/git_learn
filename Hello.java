4649e14c0ce6e893ee17f042a5878054e0a4fe51
wujianxiang <bestyoooo@163.com>
2023-09-05 11:27:34 +0800

    master new merge

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello {
 	System.out.println("Hello world!");
 	System.out.println("Hello hake");
 	System.out.println("Dev Hello");
+	System.out.println("Master new merge");
     }
 }
 
