f0534fe95859d76cd8e2d9424270345e743f87de
wujianxiang <bestyoooo@163.com>
2023-09-05 11:23:12 +0800

    dev commit

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
 	System.out.println("Hello world!");
 	System.out.println("Hello hake");
+	System.out.println("Dev Hello");
     }
 }
 
